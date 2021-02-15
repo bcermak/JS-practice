1effadf6b35c0945323561b331ee0126c6dad603
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-14 21:18:30 -0600

    added button and some style

--- basic/index.js
+++ basic/index.js
@@ -1,13 +1,30 @@
-let newDiv = document.createElement('div');
-newDiv.innerHTML = '<p> Hellllllllo World </p>';
+const header = document.querySelector('h2');
+
+header.style = 'text-align: center;'
+
+const newDiv = document.createElement('div');
 
 document.body.appendChild(newDiv);
+
+newDiv.setAttribute('style', 'text-align: center');
 
 function addInput () {
     let input = document.createElement('input');
     input.id = 'dynamicInput';
     input.type = 'text';
+    input.name = 'hello';
+    input.placeholder = "Hello";
     newDiv.appendChild(input);
 }
 
 addInput ();
+
+function addBtn () {
+    let btn = document.createElement('button');
+    btn.className = 'btn btn-primary';
+    btn.innerHTML = "Hello World";
+    btn.setAttribute('style', "margin-left: 10px;")
+    newDiv.appendChild(btn);
+};
+
+addBtn ();
