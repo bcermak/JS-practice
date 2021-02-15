5e6847a326453734597de8d2a976b6205aa5542e
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-14 20:55:15 -0600

    added form dynamically

--- basic/index.js
+++ basic/index.js
@@ -1,4 +1,13 @@
 let newDiv = document.createElement('div');
 newDiv.innerHTML = '<p> Hellllllllo World </p>';
 
-document.body.appendChild(newDiv)
+document.body.appendChild(newDiv);
+
+function addInput () {
+    let input = document.createElement('input');
+    input.id = 'dynamicInput';
+    input.type = 'text';
+    newDiv.appendChild(input);
+}
+
+addInput ();
